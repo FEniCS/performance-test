a905e9c3af8ec3f011b21c32743d93ea6391e8fa
Chris Richardson <chris@bpi.cam.ac.uk>
2019-01-15 14:01:57 +0000

    allow unknown options through

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,12 @@ int main(int argc, char* argv[])
       "number of degrees of freedom");
 
   po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::store(po::command_line_parser(argc, argv)
+                .options(desc)
+                .allow_unregistered()
+                .run(),
+            vm);
+  po::notify(vm);
 
   if (vm.count("help"))
   {
