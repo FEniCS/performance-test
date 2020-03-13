e652fbfe7b5520983f194126c174d0b674d7186c
Chris Richardson <chris@bpi.cam.ac.uk>
2020-03-13 14:52:02 +0000

    Chris/update for permutation (#47)
    * Move to correct location
    
    * Tidy with clang format
    
    * Fix arch flag
    
    * Create connectivity
    
    * Also for unstruct mesh
    
    * Right name
    
    * Change dimension

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -107,6 +107,7 @@ std::shared_ptr<dolfinx::mesh::Mesh> create_cube_mesh(MPI_Comm comm,
 
   for (int i = 0; i < r; ++i)
   {
+    mesh->create_connectivity(3, 1);
     mesh = std::make_shared<dolfinx::mesh::Mesh>(
         dolfinx::refinement::refine(*mesh, false));
   }
@@ -284,6 +285,7 @@ create_spoke_mesh(MPI_Comm comm, std::size_t target_dofs,
     for (int i = 0; i < mesh->num_entities(1); ++i)
       marker_array[i] = (i % 2000 < nmarked);
 
+    mesh->create_connectivity(1, 1);
     meshi = std::make_shared<dolfinx::mesh::Mesh>(
         dolfinx::refinement::refine(*mesh, marker, false));
 
