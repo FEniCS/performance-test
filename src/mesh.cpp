9315af23a89ca63b87eece444ec407c878be2e4f
Chris Richardson <chris@bpi.cam.ac.uk>
2020-03-23 13:58:37 +0000

    Mesh constructor (#50)
    * Mesh constructor update

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -243,7 +243,8 @@ create_spoke_mesh(MPI_Comm comm, std::size_t target_dofs,
   const dolfinx::fem::ElementDofLayout layout = dolfinx::fem::geometry_layout(
       dolfinx::mesh::CellType::tetrahedron, topo.cols());
   auto mesh = std::make_shared<dolfinx::mesh::Mesh>(dolfinx::mesh::create(
-      comm, dolfinx::graph::AdjacencyList<std::int64_t>(topo), layout, geom));
+      comm, dolfinx::graph::AdjacencyList<std::int64_t>(topo), layout, geom,
+      dolfinx::mesh::GhostMode::none));
 
   mesh->create_entities(1);
 
