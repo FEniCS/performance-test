c2bdca80735659850e3998f6d8f515abb5a90a25
Jeffrey Salmond <jeffrey@cambridgesupercomputing.com>
2020-11-05 12:42:35 +0000

    add dolfinx git string to output

--- src/main.cpp
+++ src/main.cpp
@@ -140,6 +140,7 @@ int main(int argc, char* argv[])
         << std::endl;
     std::cout << "Test problem summary" << std::endl;
     std::cout << "  dolfinx version: " << DOLFINX_VERSION_STRING << std::endl;
+    std::cout << "  dolfinx git:     " << DOLFINX_VERSION_GIT << std::endl;
     std::cout << "  Problem type:    " << problem_type << std::endl;
     std::cout << "  Scaling type:    " << scaling_type << std::endl;
     std::cout << "  Num processes:   " << num_processes << std::endl;
