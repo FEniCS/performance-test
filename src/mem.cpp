c2b685c4ec459768e22c7f84184706ca115761d6
Chris Richardson <chris@bpi.cam.ac.uk>
2021-07-12 10:29:36 +0100

    more mem profile work

--- src/mem.cpp
+++ src/mem.cpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2021 Chris N. Richardson
+//
+// This file is part of FEniCS-miniapp (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    MIT
+
 #include <vector>
 #include <unistd.h>
 #include <ios>
@@ -11,6 +17,7 @@
 
 void process_mem_usage(bool& quit)
 {
+  loguru::set_thread_name("MEMORY");
   const int page_size_bytes = sysconf(_SC_PAGE_SIZE);
     
   while(!quit)
