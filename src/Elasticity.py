e106f0f7442a041a88ad45ffc7e6732246d4617e
Garth N. Wells <gnw20@cam.ac.uk>
2022-02-21 10:23:05 +0000

    UFL/FFCx update (#123)

--- src/Elasticity.py
+++ src/Elasticity.py
@@ -1,8 +1,11 @@
-# Copyright (C) 2017-2019 Chris N. Richardson and Garth N. Wells
+# Copyright (C) 2017-2022 Chris N. Richardson and Garth N. Wells
 #
 # This file is part of FEniCS-miniapp (https://www.fenicsproject.org)
 #
 # SPDX-License-Identifier:    MIT
+
+from ufl import (Coefficient, Identity, TestFunction, TrialFunction,
+                 VectorElement, dx, grad, inner, tetrahedron, tr)
 
 # Elasticity parameters
 E = 1.0e6
@@ -20,16 +23,16 @@ for degree in range(1, 4):
     u, v = TrialFunction(element), TestFunction(element)
     f = Coefficient(element)
 
-    def epsilon(v):
+    def eps(v):
         return 0.5*(grad(v) + grad(v).T)
 
     def sigma(v):
-        return 2.0*mu*epsilon(v) + lmbda*tr(epsilon(v))*Identity(v.geometric_dimension())
+        return 2.0*mu*eps(v) + lmbda*tr(eps(v))*Identity(v.geometric_dimension())
 
     # Add forms to namespace with names a1, a2, a3 etc.
     aname = 'a' + str(degree)
     Lname = 'L' + str(degree)
-    ns[aname] = inner(sigma(u), epsilon(v))*dx
+    ns[aname] = inner(sigma(u), eps(v))*dx
     ns[Lname] = inner(f, v)*dx
 
     del u, v, f
