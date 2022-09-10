0086b9938f0befecf27139a4e0c39f35f5115dfa
Garth N. Wells <gnw20@cam.ac.uk>
2022-09-10 14:33:34 +0100

    Updates for pending xtensor removal from DOLFINx. (#133)

--- src/poisson_problem.cpp
+++ src/poisson_problem.cpp
@@ -21,16 +21,12 @@
 #include <memory>
 #include <petscsys.h>
 #include <utility>
-#include <xtensor/xarray.hpp>
-#include <xtensor/xmath.hpp>
-#include <xtensor/xview.hpp>
 
 using namespace dolfinx;
+using T = PetscScalar;
 
-std::tuple<std::shared_ptr<la::Vector<PetscScalar>>,
-           std::shared_ptr<fem::Function<PetscScalar>>,
-           std::function<int(fem::Function<PetscScalar>&,
-                             const la::Vector<PetscScalar>&)>>
+std::tuple<std::shared_ptr<la::Vector<T>>, std::shared_ptr<fem::Function<T>>,
+           std::function<int(fem::Function<T>&, const la::Vector<T>&)>>
 poisson::problem(std::shared_ptr<mesh::Mesh> mesh, int order)
 {
   common::Timer t0("ZZZ FunctionSpace");
@@ -48,40 +44,59 @@ poisson::problem(std::shared_ptr<mesh::Mesh> mesh, int order)
 
   common::Timer t2("ZZZ Create boundary conditions");
   // Define boundary condition
-  auto u0 = std::make_shared<fem::Function<PetscScalar>>(V);
+  auto u0 = std::make_shared<fem::Function<T>>(V);
   u0->x()->set(0);
 
   // Find facets with bc applied
   const int tdim = mesh->topology().dim();
   const std::vector<std::int32_t> bc_facets = mesh::locate_entities(
       *mesh, tdim - 1,
-      [](const xt::xtensor<double, 2>& x) -> xt::xtensor<bool, 1>
+      [](auto x)
       {
-        auto x0 = xt::row(x, 0);
-        return xt::isclose(x0, 0.0) or xt::isclose(x0, 1.0);
+        constexpr double eps = 1.0e-8;
+        std::vector<std::int8_t> marker(x.extent(1), false);
+        for (std::size_t p = 0; p < x.extent(1); ++p)
+        {
+          double x0 = x(0, p);
+          if (std::abs(x0) < eps or std::abs(x0 - 1) < eps)
+            marker[p] = true;
+        }
+        return marker;
       });
 
   // Find constrained dofs
   const std::vector<std::int32_t> bdofs
       = fem::locate_dofs_topological(*V, tdim - 1, bc_facets);
 
-  auto bc = std::make_shared<fem::DirichletBC<PetscScalar>>(u0, bdofs);
+  auto bc = std::make_shared<fem::DirichletBC<T>>(u0, bdofs);
   t2.stop();
 
   // Define coefficients
   common::Timer t3("ZZZ Create RHS function");
-  auto f = std::make_shared<fem::Function<PetscScalar>>(V);
-  auto g = std::make_shared<fem::Function<PetscScalar>>(V);
+  auto f = std::make_shared<fem::Function<T>>(V);
+  auto g = std::make_shared<fem::Function<T>>(V);
   f->interpolate(
-      [](const xt::xtensor<double, 2>& x) -> xt::xarray<PetscScalar>
+      [](auto x) -> std::pair<std::vector<T>, std::vector<std::size_t>>
       {
-        auto dx
-            = xt::square(xt::row(x, 0) - 0.5) + xt::square(xt::row(x, 1) - 0.5);
-        return 10 * xt::exp(-(dx) / 0.02);
-      });
+        std::vector<T> v(x.extent(1));
+        for (std::size_t p = 0; p < x.extent(1); ++p)
+        {
+          double dx = x(0, p) - 0.5;
+          double dy = x(1, p) - 0.5;
+          double dr = dx * dx + dy * dy;
+          v[p] = 10 * std::exp(-dr / 0.02);
+        }
 
-  g->interpolate([](const xt::xtensor<double, 2>& x) -> xt::xarray<PetscScalar>
-                 { return xt::sin(5.0 * xt::row(x, 0)); });
+        return {std::move(v), {v.size()}};
+      });
+  g->interpolate(
+      [](auto x) -> std::pair<std::vector<T>, std::vector<std::size_t>>
+      {
+        std::vector<T> f(x.extent(1));
+        for (std::size_t p = 0; p < x.extent(1); ++p)
+          f[p] = std::sin(5 * x(0, p));
+        return {f, {f.size()}};
+      });
   t3.stop();
 
   std::vector form_poisson_L
@@ -90,14 +105,11 @@ poisson::problem(std::shared_ptr<mesh::Mesh> mesh, int order)
       = {form_Poisson_a1, form_Poisson_a2, form_Poisson_a3};
 
   // Define variational forms
-  auto L
-      = std::make_shared<fem::Form<PetscScalar>>(fem::create_form<PetscScalar>(
-          *form_poisson_L.at(order - 1), {V}, {{"w0", f}, {"w1", g}}, {}, {}));
-  auto a
-      = std::make_shared<fem::Form<PetscScalar>>(fem::create_form<PetscScalar>(
-          *form_poisson_a.at(order - 1), {V, V},
-          std::vector<std::shared_ptr<const fem::Function<PetscScalar>>>{}, {},
-          {}));
+  auto L = std::make_shared<fem::Form<T>>(fem::create_form<T>(
+      *form_poisson_L.at(order - 1), {V}, {{"w0", f}, {"w1", g}}, {}, {}));
+  auto a = std::make_shared<fem::Form<T>>(fem::create_form<T>(
+      *form_poisson_a.at(order - 1), {V, V},
+      std::vector<std::shared_ptr<const fem::Function<T>>>{}, {}, {}));
 
   // Create matrices and vector, and assemble system
   std::shared_ptr<la::petsc::Matrix> A = std::make_shared<la::petsc::Matrix>(
@@ -107,27 +119,27 @@ poisson::problem(std::shared_ptr<mesh::Mesh> mesh, int order)
   const std::vector constants_a = fem::pack_constants(*a);
   auto coeffs_a = fem::allocate_coefficient_storage(*a);
   fem::pack_coefficients(*a, coeffs_a);
-  fem::assemble_matrix<PetscScalar>(
-      la::petsc::Matrix::set_block_fn(A->mat(), ADD_VALUES), *a,
-      tcb::make_span(constants_a), fem::make_coefficients_span(coeffs_a), {bc});
+  fem::assemble_matrix<T>(la::petsc::Matrix::set_block_fn(A->mat(), ADD_VALUES),
+                          *a, constants_a,
+                          fem::make_coefficients_span(coeffs_a), {bc});
   MatAssemblyBegin(A->mat(), MAT_FLUSH_ASSEMBLY);
   MatAssemblyEnd(A->mat(), MAT_FLUSH_ASSEMBLY);
-  fem::set_diagonal<PetscScalar>(
-      la::petsc::Matrix::set_fn(A->mat(), INSERT_VALUES), *V, {bc});
+  fem::set_diagonal<T>(la::petsc::Matrix::set_fn(A->mat(), INSERT_VALUES), *V,
+                       {bc});
   MatAssemblyBegin(A->mat(), MAT_FINAL_ASSEMBLY);
   MatAssemblyEnd(A->mat(), MAT_FINAL_ASSEMBLY);
   t4.stop();
 
   // Create la::Vector
-  la::Vector<PetscScalar> b(L->function_spaces()[0]->dofmap()->index_map,
-                            L->function_spaces()[0]->dofmap()->index_map_bs());
+  la::Vector<T> b(L->function_spaces()[0]->dofmap()->index_map,
+                  L->function_spaces()[0]->dofmap()->index_map_bs());
   b.set(0);
   common::Timer t5("ZZZ Assemble vector");
   const std::vector constants_L = fem::pack_constants(*L);
   auto coeffs_L = fem::allocate_coefficient_storage(*L);
   fem::pack_coefficients(*L, coeffs_L);
-  fem::assemble_vector<PetscScalar>(b.mutable_array(), *L, constants_L,
-                                    fem::make_coefficients_span(coeffs_L));
+  fem::assemble_vector<T>(b.mutable_array(), *L, constants_L,
+                          fem::make_coefficients_span(coeffs_L));
   fem::apply_lifting(b.mutable_array(), {a}, {constants_L},
                      {fem::make_coefficients_span(coeffs_L)}, {{bc}}, {}, 1.0);
   b.scatter_rev(std::plus<>());
@@ -137,11 +149,9 @@ poisson::problem(std::shared_ptr<mesh::Mesh> mesh, int order)
   t1.stop();
 
   // Create Function to hold solution
-  auto u = std::make_shared<fem::Function<PetscScalar>>(V);
-  std::function<int(fem::Function<PetscScalar>&,
-                    const la::Vector<PetscScalar>&)>
-      solver_function
-      = [A](fem::Function<PetscScalar>& u, const la::Vector<PetscScalar>& b)
+  auto u = std::make_shared<fem::Function<T>>(V);
+  std::function<int(fem::Function<T>&, const la::Vector<T>&)> solver_function
+      = [A](fem::Function<T>& u, const la::Vector<T>& b)
   {
     // Create solver
     la::petsc::KrylovSolver solver(MPI_COMM_WORLD);
@@ -157,6 +167,5 @@ poisson::problem(std::shared_ptr<mesh::Mesh> mesh, int order)
     return num_iter;
   };
 
-  return {std::make_shared<la::Vector<PetscScalar>>(std::move(b)), u,
-          solver_function};
+  return {std::make_shared<la::Vector<T>>(std::move(b)), u, solver_function};
 }
