2bf65e86da41fa044151845c7a29e0e335c7602d
Chris Richardson <chris@bpi.cam.ac.uk>
2022-12-12 11:37:19 +0000

    Fix CMakelists

---
+++ src/env.sh
@@ -0,0 +1,3 @@
+export CATALYST_IMPLEMENTATION_NAME=paraview
+export CATALYST_IMPLEMENTATION_PATHS=/rds/user/cnr12/hpc-work/spack/install/linux-rocky8-icelake/gcc-11.2.0/paraview-5.10.1-qz35cu55u442s23ixfdvxbgb4r4vqpjm/lib64/catalyst
+
