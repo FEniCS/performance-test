6a77205a5c98fcb2b8f901535be060186fd7fc87
Chris Richardson <cnr12@cam.ac.uk>
2025-02-25 13:33:20 +0000

    Add copyright

--- gpu/src/mesh.hpp
+++ gpu/src/mesh.hpp
@@ -1,3 +1,6 @@
+// Copyright(C) 2023-2025 Igor A. Baratta, Chris N. Richardson, Joseph P. Dean,
+// Garth N. Wells
+// SPDX-License-Identifier:    MIT
 
 #pragma once
 
@@ -10,14 +13,16 @@
 #include <map>
 #include <span>
 
-/// @brief Create a new mesh with an extra boundary layer, such that all cells on other processes
-/// which share a vertex with this process are ghosted.
+/// @brief Create a new mesh with an extra boundary layer, such that all cells
+/// on other processes which share a vertex with this process are ghosted.
 /// @param mesh Input mesh
-/// @param coord_element A coordinate element for the new mesh. This may be tensor product ordering.
+/// @param coord_element A coordinate element for the new mesh. This may be
+/// tensor product ordering.
 
 template <std::floating_point T>
-dolfinx::mesh::Mesh<T> ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
-                                        dolfinx::fem::CoordinateElement<T> coord_element)
+dolfinx::mesh::Mesh<T>
+ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
+                 dolfinx::fem::CoordinateElement<T> coord_element)
 {
   constexpr int tdim = 3;
   constexpr int gdim = 3;
@@ -25,7 +30,8 @@ dolfinx::mesh::Mesh<T> ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
   std::size_t num_vertices = mesh.topology()->index_map(0)->size_local();
 
   // Find which local vertices are ghosted elsewhere
-  auto vertex_destinations = mesh.topology()->index_map(0)->index_to_dest_ranks();
+  auto vertex_destinations
+      = mesh.topology()->index_map(0)->index_to_dest_ranks();
 
   // Map from any local cells to processes where they should be ghosted
   std::map<int, std::vector<int>> cell_to_dests;
@@ -50,9 +56,10 @@ dolfinx::mesh::Mesh<T> ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
   spdlog::info("cell_to_dests= {}, ncells = {}", cell_to_dests.size(), ncells);
 
   auto partitioner
-      = [cell_to_dests, ncells](MPI_Comm comm, int nparts,
-                                const std::vector<dolfinx::mesh::CellType>& cell_types,
-                                const std::vector<std::span<const std::int64_t>>& cells)
+      = [cell_to_dests,
+         ncells](MPI_Comm comm, int nparts,
+                 const std::vector<dolfinx::mesh::CellType>& cell_types,
+                 const std::vector<std::span<const std::int64_t>>& cells)
   {
     int rank = dolfinx::MPI::rank(comm);
     std::vector<std::int32_t> dests;
@@ -66,7 +73,8 @@ dolfinx::mesh::Mesh<T> ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
       // Ghost to other processes
       offsets.push_back(dests.size());
     }
-    return dolfinx::graph::AdjacencyList<std::int32_t>(std::move(dests), std::move(offsets));
+    return dolfinx::graph::AdjacencyList<std::int32_t>(std::move(dests),
+                                                       std::move(offsets));
   };
 
   std::array<std::size_t, 2> xshape = {num_vertices, gdim};
@@ -76,8 +84,10 @@ dolfinx::mesh::Mesh<T> ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
   auto imap = mesh.geometry().index_map();
   std::vector<std::int32_t> permuted_dofmap;
   std::vector<int> perm = basix::tp_dof_ordering(
-      basix::element::family::P, dolfinx::mesh::cell_type_to_basix_type(coord_element.cell_shape()),
-      coord_element.degree(), coord_element.variant(), basix::element::dpc_variant::unset, false);
+      basix::element::family::P,
+      dolfinx::mesh::cell_type_to_basix_type(coord_element.cell_shape()),
+      coord_element.degree(), coord_element.variant(),
+      basix::element::dpc_variant::unset, false);
   for (std::size_t c = 0; c < dofmap.extent(0); ++c)
   {
     auto cell_dofs = std::submdspan(dofmap, c, std::full_extent);
@@ -87,9 +97,9 @@ dolfinx::mesh::Mesh<T> ghost_layer_mesh(dolfinx::mesh::Mesh<T>& mesh,
   std::vector<std::int64_t> permuted_dofmap_global(permuted_dofmap.size());
   imap->local_to_global(permuted_dofmap, permuted_dofmap_global);
 
-  auto new_mesh
-      = dolfinx::mesh::create_mesh(mesh.comm(), mesh.comm(), std::span(permuted_dofmap_global),
-                                   coord_element, mesh.comm(), x, xshape, partitioner);
+  auto new_mesh = dolfinx::mesh::create_mesh(
+      mesh.comm(), mesh.comm(), std::span(permuted_dofmap_global),
+      coord_element, mesh.comm(), x, xshape, partitioner);
 
   spdlog::info("** NEW MESH num_ghosts_cells = {}",
                new_mesh.topology()->index_map(tdim)->num_ghosts());
@@ -139,7 +149,8 @@ compute_boundary_cells(std::shared_ptr<dolfinx::fem::FunctionSpace<T>> V)
       local_cells.push_back(i);
   }
 
-  spdlog::debug("lcells:{}, bcells:{}", local_cells.size(), boundary_cells.size());
+  spdlog::debug("lcells:{}, bcells:{}", local_cells.size(),
+                boundary_cells.size());
 
   return {std::move(local_cells), std::move(boundary_cells)};
 }
