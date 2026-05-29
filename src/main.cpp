bf4d4784dfc6b95f917554a954f1edc7384bb457
Jack S. Hale <mail@jackhale.co.uk>
2026-05-29 13:31:28 +0200

    Remove Dockerfile and deprecated -Ofast (#172)

--- src/main.cpp
+++ src/main.cpp
@@ -264,7 +264,8 @@ int main(int argc, char* argv[])
   std::string fmt = "[%Y-%m-%d %H:%M:%S.%e] [" + thread_name + "] [%l] %v";
   spdlog::set_pattern(fmt);
 
-  // Turn off logging except on rank 0, if set (e.g. use SPDLOG_LEVEL=info in CLI)
+  // Turn off logging except on rank 0, if set (e.g. use SPDLOG_LEVEL=info in
+  // CLI)
   if (mpi_rank != 0)
     spdlog::set_level(spdlog::level::err);
 
